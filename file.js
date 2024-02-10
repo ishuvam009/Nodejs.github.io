9ab3e1465b6b19a80d45730560c4c2a174e487cc
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-11 01:33:44 +0530

    Initial Commit

--- file.js
+++ file.js
@@ -9,5 +9,27 @@ const fs = require("fs");
 
 
 // Read File Synchronous.
-const result = fs.readFileSync("./read.txt", "utf8");
-console.log(result);
+// const result = fs.readFileSync("./read.txt", "utf8");
+// console.log(result);
+
+//Read File Asynchronous
+
+// fs.readFile("./read.txt","utf8",(err,result) => {
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(result);
+//     }
+// });
+
+
+//Append File Synchronous
+fs.appendFileSync("./read.txt",`\n${Date.now()}, Hey There!`);
+
+// //Copy a file.
+// fs.cpSync("./read.txt","./read2copy.txt");
+
+// //Delete a file.
+// fs.unlinkSync("./read2copy.txt");
+
+

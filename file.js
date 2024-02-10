8b2fcf10e00084832c1f5e0759dfc84d779305cc
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-11 00:03:28 +0530

    done

--- file.js
+++ file.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 //Synchronous
 fs.writeFileSync("./test.txt", "Hey There!");
 
+
 //Asynchronous
-fs.writeFile("./test2.txt", "Hey There! Asynchronous", (err) =>{
-    consolelog(err);
-});
+fs.writeFile("./test2.txt", "Hey There! Asynchronous yehh hh", (err) =>{});
+

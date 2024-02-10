07c36b3c4d38a9f597dfcb4fc73a45e61e1d6425
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-11 00:00:21 +0530

    Initial Commmit

--- file.js
+++ file.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
 
+//Synchronous
 fs.writeFileSync("./test.txt", "Hey There!");
 
+//Asynchronous
+fs.writeFile("./test2.txt", "Hey There! Asynchronous", (err) =>{
+    consolelog(err);
+});

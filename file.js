c454ca06df946483c28701111b9f8f1d2b5f7d12
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-11 01:01:23 +0530

    Initial Commit

--- file.js
+++ file.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
 
 //Synchronous
-fs.writeFileSync("./test.txt", "Hey There!");
+//fs.writeFileSync("./test.txt", "Hey There!");
 
 
 //Asynchronous
-fs.writeFile("./test2.txt", "Hey There! Asynchronous yehh hh", (err) =>{});
+//fs.writeFile("./test2.txt", "Hey There! Asynchronous yehh hh", (err) =>{});
 
+
+// Read File Synchronous.
+const result = fs.readFileSync("./read.txt", "utf8");
+console.log(result);

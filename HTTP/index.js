cf8197a376651144e406909dbaa070fb9230c79a
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-12 22:41:03 +0530

    INITIAL COMMIT

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,8 +1,30 @@
+//Multiple Routes
+//Logging the request in a file
+//Using fs module to write the log in a file
+//Using appendFile method to append the log in the file
+//Using switch case to handle multiple routes
+//Using req.url to get the requested url
+
+
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req,res) => {
-    console.log("New Req Receive.");
-    res.end("Hello from server.");
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    fs.appendFile("./Log.txt", log, (err, data) => {
+
+        switch(req.url){
+            case '/': res.end("Home Page");
+            break
+            case '/about': res.end("Hi I am Shuvam Mandal");
+            break
+            default: res.end("404 Page not found.");
+        }
+        //res.end("Hello from server, Again.");
+    });
+
+    //console.log(req.headers);
+    
 });
 
 myServer.listen(9000, () => console.log("Server is running on port 9000"));

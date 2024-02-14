03dbc8e4ca4ac3c594b6eddcfa0408dbc2308ade
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-15 00:19:12 +0530

    Initial Commit

--- URL/index.js
+++ URL/index.js
@@ -2,7 +2,18 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req,res) => {
-
+    switch(req.url) {
+        case "/" :
+            res.end("Home Page");
+        break;
+        
+        case "/about" :
+            res.end("Hi i am Shuvam Mandal");
+        break;
+        
+        case "/contact" :
+            res.end("Contact me at shuuvam.mandal09@gmail.com");
+    }
 } )
 
-myServer.listen(7000, () => console.log("Server is strated at 7000."));
+myServer.listen(3000, () => console.log("Server is strated at 3000."));

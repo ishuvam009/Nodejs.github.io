38166fc539efbe1286a97029363555e24790980c
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-14 23:58:10 +0530

    Initial Commit

--- URL/index.js
+++ URL/index.js
@@ -1,3 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 
+const myServer = http.createServer((req,res) => {
+
+} )
+
+myServer.listen(7000, () => console.log("Server is strated at 7000."));

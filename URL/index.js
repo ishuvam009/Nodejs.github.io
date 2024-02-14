569ddd6d76a7393b7bc86306ab976166e2a9c29b
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-15 00:57:39 +0530

    Initial Commit

--- URL/index.js
+++ URL/index.js
@@ -2,17 +2,51 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req,res) => {
-    switch(req.url) {
-        case "/" :
-            res.end("Home Page");
-        break;
+
+    
+    // switch(req.url) {
+    //     case "/" :
+    //         res.end("Home Page");
+    //     break;
         
-        case "/about" :
-            res.end("Hi i am Shuvam Mandal");
-        break;
+    //     case "/about" :
+    //         res.end("Hi i am Shuvam Mandal");
+    //     break;
         
-        case "/contact" :
-            res.end("Contact me at shuuvam.mandal09@gmail.com");
+    //     case "/contact" :
+    //         res.end("Contact me at shuuvam.mandal09@gmail.com");
+    // }
+
+    if(req.url === "/") {
+        fs.readFile("index.html", "utf-8", (err, data) => {
+            if(err) {
+                res.end("Error");
+            } else {
+                res.end(data);
+            }
+        })  
+    }
+
+    if(req.url === "/about"){
+        fs.readFile("about.html", "utf-8", (err, data) => {
+            if(err){
+                res.end(err);
+            } 
+            else{
+                res.end(data);
+            }
+        })
+    }
+
+    if(req.url === "/contact"){
+        fs.readFile("contact.html","utf-8",(err,data) => {
+            if(err){
+                res.end(err);
+            }
+            else{
+                res.end(data);
+            }
+        })
     }
 } )
 

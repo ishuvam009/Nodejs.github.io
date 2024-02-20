7351db8893bc92958ba6397f96419ff70928fdc7
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-21 01:35:44 +0530

    Commit

--- 1/index.js
+++ 1/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 //const mongoose = require('mongoose');
-
 const app = express();
 const port = 5000;
 
+//Getting the environment variables
 dotenv.config({path: './config.env'});
 
-
+//Getting the DB Connection
 require('./DB/db')
 
 app.get('/', (req,res) => {
